fdd6a143a75415a8284063d3d1b5333b62a0c63a
Mirko Lenz <info@mirko-lenz.de>
2019-12-11 21:31:06 +0100

    Remove pos tags and improve code

--- knowledge_graph/convert.py
+++ knowledge_graph/convert.py
@@ -3,12 +3,6 @@ ConceptNet CSV Dump: https://github.com/commonsense/conceptnet5/wiki/Downloads
 Source Code: https://github.com/tomkdickinson/conceptnet_neo4j
 Import to Neo4j: https://neo4j.com/docs/operations-manual/current/tutorial/import-tool/
 
-NOTE: Currently, URI is exported too which results in duplicate property entries.
-If the terms without the prefix and suffix are unique, one could remove the uri and replace it with the name.
-
-TODO: Node de/n/n is duplicate
-TODO: Loops on one node possible as lang, name, pos not unique in conceptnet
-
 >>> neo4j-admin import --database "concept.db" --nodes "nodes.csv" --relationships "relationships.csv"
 """
 
@@ -20,7 +14,7 @@ from typing import List, Dict
 import gzip
 import ast
 import click
-from . import uri
+from .uri import split_uri, concept_uri, is_concept, is_relation
 
 
 lang_filter = ["de", "en"]
@@ -35,62 +29,48 @@ lang_filter = ["de", "en"]
 
 @dataclass(frozen=True)
 class Node:
-    prefix: str
     language: str
     name: str
-    pos: str
 
     @classmethod
-    def create(cls, uri: str) -> "Node":
-        uri = uri.split("/")
+    def from_uri(cls, uri: str) -> "Node":
+        uri = split_uri(uri)
 
-        return cls(uri[1], uri[2], uri[3], uri[4] if len(uri) > 4 else None)
+        return cls(uri[1], uri[2])
 
     @property
     def key(self):
-        return "/".join(filter(None, (self.language, self.name, self.pos)))
+        return concept_uri(self.language, self.name)
 
     @property
     def label(self):
-        return "Concept"
-
-
-@dataclass(frozen=True)
-class Source:
-    contributor: str
-    process: str
+        return "Conceptnet"
 
 
 @dataclass(frozen=True)
 class Relationship:
-    prefix: str
     name: str
     start: Node
     end: Node
     dataset: str
     license: str
     weight: float
-    # sources: List[Source]
 
     @classmethod
-    def create(cls, uri: str, metadata: str, start: Node, end: Node) -> "Relationship":
-        uri = uri.split("/")
+    def from_uri(
+        cls, uri: str, metadata: str, start: Node, end: Node
+    ) -> "Relationship":
+        uri = split_uri(uri)
         metadata = ast.literal_eval(metadata)
 
         return cls(
             uri[1],
-            uri[2],
             start,
             end,
             metadata.get("dataset"),
             metadata.get("license"),
             float(metadata.get("weight", 1.0)),
         )
-
-        # self.sources=[
-        #     Source(source.get("contributor", ""), source.get("process", ""))
-        #     for source in metadata["sources"]
-        # ],
 
 
 @click.command()
@@ -99,7 +79,7 @@ class Relationship:
 @click.argument("relationships_csv", default="/var/lib/neo4j/import/relationships.csv")
 # @click.option("break_after", default=0)
 def run(conceptnet_csv: str, nodes_csv: str, relationships_csv: str) -> None:
-    nodes = dict()
+    nodes = set()
     relationships = set()
 
     with gzip.open(conceptnet_csv, "rt") as f:
@@ -107,59 +87,31 @@ def run(conceptnet_csv: str, nodes_csv: str, relationships_csv: str) -> None:
         print(f"Reading {conceptnet_csv}")
 
         for i, row in enumerate(rows):
-            start = Node.create(row[2])
-            end = Node.create(row[3])
-            rel = Relationship.create(row[1], row[4], start, end)
+            rel_uri = row[1]
+            start_uri = row[2]
+            end_uri = row[3]
 
-            if (
-                start.prefix == "c"
-                and end.prefix == "c"
-                and rel.prefix == "r"
-                and start.language in lang_filter
-                and end.language in lang_filter
-            ):
-                if start.key not in nodes:
-                    nodes[start.key] = start
+            if is_concept(start_uri) and is_concept(end_uri) and is_relation(rel_uri):
+                start = Node.from_uri(start_uri)
+                end = Node.from_uri(end_uri)
 
-                if end.key not in nodes:
-                    nodes[end.key] = end
-
-                relationships.add(rel)
-
-                if start.pos:
-                    start_general = nodes.get(start.key) or Node(
-                        start.prefix, start.language, start.name, None
-                    )
-                    rel_start_general = Relationship.create(
-                        row[1], row[4], start_general, end
-                    )
-
-                    nodes[start_general.key] = start_general
-                    relationships.add(rel_start_general)
-
-                if end.pos:
-                    end_general = nodes.get(end.key) or Node(
-                        end.prefix, end.language, end.name, None
-                    )
-                    rel_end_general = Relationship.create(
-                        row[1], row[4], start, end_general
-                    )
-
-                    nodes[end_general] = end_general
-                    relationships.add(rel_end_general)
-
-                if start.pos and end.pos:
-                    relationships.add(
-                        Relationship.create(row[1], row[4], start_general, end_general)
-                    )
+                if (
+                    start.language in lang_filter
+                    and end.language in lang_filter
+                    and start != end
+                ):
+                    relationship = Relationship.from_uri(rel_uri, row[4], start, end)
+                    nodes.add(start)
+                    nodes.add(end)
+                    relationships.add(relationship)
 
     with open(nodes_csv, "w") as f:
         writer = csv.writer(f)
         print(f"Writing {nodes_csv}")
-        writer.writerow(["key:ID", ":LABEL", "name", "language", "pos"])
+        writer.writerow(["key:ID", ":LABEL", "name", "language"])
 
         for n in nodes.values():
-            writer.writerow((n.key, n.label, n.name, n.language, n.pos))
+            writer.writerow((n.key, n.label, n.name, n.language))
 
     with open(relationships_csv, "w") as f:
         writer = csv.writer(f)
@@ -170,7 +122,7 @@ def run(conceptnet_csv: str, nodes_csv: str, relationships_csv: str) -> None:
 
         for r in relationships:
             writer.writerow(
-                (r.start.key, r.end.key, r.name, r.dataset, r.license, r.weight,)
+                (r.start.key, r.end.key, r.name, r.dataset, r.license, r.weight)
             )
 
 
