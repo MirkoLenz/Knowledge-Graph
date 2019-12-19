8cd080f9f8f924195272c0e2e04a70433de2f0d0
Mirko Lenz <info@mirko-lenz.de>
2019-12-19 15:32:50 +0100

    Rename script to import

---
+++ import.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+export NEO4J_HOME=/data/knowledge-graph/neo4j
+docker stop recap-neo4j
+rm -rf $NEO4J_HOME/data/databases
+docker run --name recap-neo4j --rm -v $NEO4J_HOME/data:/data -v $NEO4J_HOME/logs:/logs -v $NEO4J_HOME/import:/import neo4j bin/neo4j-admin import --nodes "import/conceptnet-nodes.csv" --relationships "import/conceptnet-relationships.csv"
+docker run --name recap-neo4j --rm -p7474:7474 -p7687:7687 -d -v $NEO4J_HOME/data:/data -v $NEO4J_HOME/logs:/logs -v $NEO4J_HOME/import:/import -v $NEO4J_HOME/plugins:/plugins --env NEO4J_AUTH=none --env NEO4J_dbms_connector_bolt_advertised__address=136.199.130.136:7687 neo4j
--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-export NEO4J_HOME=/data/knowledge-graph/neo4j
-sudo docker run --name recap-neo4j --rm -v $NEO4J_HOME/data:/data -v $NEO4J_HOME/logs:/logs -v $NEO4J_HOME/import:/import neo4j bin/neo4j-admin import --nodes "import/conceptnet-nodes.csv" --relationships "import/conceptnet-relationships.csv"
-sudo docker run --name recap-neo4j --rm -p7474:7474 -p7687:7687 -d -v $NEO4J_HOME/data:/data -v $NEO4J_HOME/logs:/logs -v $NEO4J_HOME/import:/import -v $NEO4J_HOME/plugins:/plugins --env NEO4J_AUTH=none --env NEO4J_dbms_connector_bolt_advertised__address=136.199.130.136:7687 neo4j
