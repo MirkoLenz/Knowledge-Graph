342dba6daae6a93066eb2c4e56c38e656aed9e2f
Mirko Lenz <info@mirko-lenz.de>
2020-09-21 21:42:20 +0200

    Use env file for neo4j auth

--- knowledge_graph/post_process.py
+++ knowledge_graph/post_process.py
@@ -9,13 +9,16 @@ DELETE n,r
 
 import click
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 from neo4j import GraphDatabase
 
 
 @click.command("post-process")
 @click.argument("neo4j_url", default="bolt://localhost:7687")
 def main(neo4j_url: str):
-    neo4j_auth = os.environ["NEO4J_AUTH"].split("/", 1)
+    neo4j_auth = os.getenv("NEO4J_AUTH").split("/", 1)
     driver = GraphDatabase.driver(neo4j_url, auth=neo4j_auth)
 
     with driver.session() as session:
